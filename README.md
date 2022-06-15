9a38c99d9888d7e4912429cc644ef4f0f1c33109
rowthan <ranronghuano1@163.com>
2022-06-15 22:00:25 +0800

    feat: 增加开发者demo

--- README.md
+++ README.md
@@ -7,7 +7,10 @@
 * vuetify
 
 ## 启动开发
+> 首先，请安装`/extension` 目录下最新版 `开发者版本 pagnote插件`。
+> 当安装好 pagenote 插件后，就如同启动了一个接口服务，然后你便可以与 `pagenote插件` 进行通信，方式如同`http`请求。
 
+以下开发流程同常见 web 项目。
 ```bash
 # install dependencies
 $ npm install
@@ -16,8 +19,9 @@ $ npm install
 $ npm run dev
 ```
 
+demo 见 `pages/users/you_nickname.vue`
 ```typescript
-// api 调用demo
+// api 调用demo。 有什么接口，参数 返回是什么，根据 typescript 提示查看即可
 import generateApi from '@pagenote/shared/lib/generateApi'
 const api = generateApi();
 
@@ -41,10 +45,14 @@ mounted(){
 
 ```
 
-## 部署服务
+## 部署管理页
 
 ### 自动部署（推荐）
-你可以基于此项目进行开发，开发完毕后提交 MR 到主分支，合并后将自动部署在 https://dev.pagenote.cn/highlight
+你可以基于此项目进行开发，开发完毕后提交 MR 到 master 分支，合并后将自动部署在 https://dev.pagenote.cn 域名下
 
 ### 自行部署
 你也可以自行部署在你自己的服务器上。部署方式见 [nuxt.js](https://www.nuxtjs.cn/) 
+
+
+## 注意事项
+* 建议使用 TypeScript
---
+++ pages/developer/you_nickname.vue
@@ -0,0 +1,53 @@
+<template>
+  <ul class='developer-page'>
+    <li v-for='webpage in webpages' :key='webpage.url'>
+      url: {{webpage.url}}
+      标记：
+      <ul>
+        <li v-for='(light,index) in webpage.plainData.steps' :key='index'>
+          {{light.text}}
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+import generateApi from '@pagenote/shared/lib/generateApi'
+import { WebPage } from '@pagenote/shared/lib/@types/data'
+const api = generateApi();
+
+export default Vue.extend({
+  name: 'YouNickname',
+  data():{
+    webpages: WebPage[],
+  }{
+    return {
+      webpages: []
+    }
+  },
+  mounted() {
+    // 查询网页笔记
+    api.lightpage.getLightPages({
+      query:{
+        deleted: false,
+      },
+      sort:{
+        'createAt': -1,
+      },
+      limit: 9999,
+      ignoreDetail: false,
+    }).then((result)=> {
+      if(result.success){
+        this.webpages = result.data.pages as WebPage[];
+      }
+    })
+  }
+})
+</script>
+<style lang='scss' scoped>
+.developer-page{
+  min-height: 660px;
+}
+</style>
