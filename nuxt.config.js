2158e6c53d49f71e62864f343d56587c221e2be9
rowthan <ranronghuano1@163.com>
2022-06-16 23:43:26 +0800

    remove public path

--- nuxt.config.js
+++ nuxt.config.js
@@ -126,7 +126,7 @@ export default {
   },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    publicPath: '/bundle/',
+    // publicPath: '/bundle/',
   },
   generate: {
     devtools: true,
