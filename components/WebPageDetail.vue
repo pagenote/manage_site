143aebf42840054fe5f29bc9b800b18b95e766e3
rowthan <ranronghuano1@163.com>
2022-07-06 21:20:21 +0800

    feat: 支持标签添加

--- components/WebPageDetail.vue
+++ components/WebPageDetail.vue
@@ -8,54 +8,58 @@
         :opacity='0.8'
       >
         <v-alert>
-          已删除该网页笔记。 <span>{{webpage.expiredAt | countdown}} 彻底删除</span>。
+          已删除该网页笔记。 <span>{{ webpage.expiredAt | countdown }} 彻底删除</span>。
         </v-alert>
         <v-btn
-          color="success"
+          color='success'
           @click='revert'
         >
           点击恢复
         </v-btn>
       </v-overlay>
       <div class='title'>
-<!--        <label-edit-->
-<!--          :text='title'-->
-<!--          placeholder='点击添加标题'-->
-<!--          @text-updated='textUpdated'-->
-<!--          @text-updated-blur='textUpdated'-->
-<!--          @text-updated-enter='textUpdated'></label-edit>-->
+        <!--        <label-edit-->
+        <!--          :text='title'-->
+        <!--          placeholder='点击添加标题'-->
+        <!--          @text-updated='textUpdated'-->
+        <!--          @text-updated-blur='textUpdated'-->
+        <!--          @text-updated-enter='textUpdated'></label-edit>-->
         <span>
-          <img class='icon' :src="webpage.icon">
-          <a :href='webpage.url || webpage.key' target='_blank'>{{webpage.title || webpage.url || webpage.key}}</a>
+          <img class='icon' :src='webpage.icon'>
+          <a :href='webpage.url || webpage.key' target='_blank'>{{ webpage.title || webpage.url || webpage.key }}</a>
         </span>
         <div class='actions'>
           <m-switch v-model='showContext'></m-switch>
           <v-btn
-            elevation="2"
+            elevation='2'
             icon
             x-small
             color='#ff8383'
             @click='deletePage'
-          ><v-icon>mdi-delete</v-icon></v-btn>
+          >
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
           <v-btn
-            elevation="2"
+            elevation='2'
             icon
             x-small
             color='#ff8383'
             @click='deletePage'
-          ><v-icon>mdi-markdown</v-icon></v-btn>
+          >
+            <v-icon>mdi-markdown</v-icon>
+          </v-btn>
         </div>
       </div>
 
       <div class='abstract'>
         <step-line
-         v-for='(light,index) in webpage.plainData.steps'
-         :key='light.id+index'
-         :dense='false'
-         :show-tip='true'
-         :show-context='showContext'
-         :light='light'
-         @edit='(step)=>onEditLight(step,index)'
+          v-for='(light,index) in webpage.plainData.steps'
+          :key='light.id+index'
+          :dense='false'
+          :show-tip='true'
+          :show-context='showContext'
+          :light='light'
+          @edit='(step)=>onEditLight(step,index)'
         />
       </div>
       <div class='snapshots'>
@@ -66,7 +70,14 @@
         </v-flex>
       </div>
       <aside class='footer'>
-        <span>修改于 {{webpage.updateAt | past}}</span>
+        <div class='tags'>
+          <vue-tags-input
+            v-model='tag'
+            :tags='tags'
+            @tags-changed='onTagsChange'
+          />
+        </div>
+        <span>修改于 {{ webpage.updateAt | past }}</span>
       </aside>
     </template>
     <div v-else>
@@ -91,6 +102,8 @@ interface Data {
   webpage?: WebPage
   title: string
   showContext: boolean
+  tag: string,
+  tags: { text: string }[],
   // [key:string]: any,
 }
 
@@ -110,72 +123,90 @@ export default Vue.extend({
       webpage: undefined,
       title: '',
       showContext: contextMode.get(),
+      tag: '',
+      tags: []
     }
   },
   watch: {
     pageKey() {
       this.getDetail()
     },
-    showContext(val){
+    showContext(val) {
       contextMode.set(val)
-    }
+    },
   },
   mounted() {
-    this.getDetail();
-    onVisibilityChange(()=>{
+    this.getDetail()
+    onVisibilityChange(() => {
       this.getDetail()
     })
   },
   methods: {
+    onTagsChange(tags:{text: string}[]){
+      this.tags = tags;
+      this.save({
+        // @ts-ignore
+        plainData:{
+          categories: tags.map(function(item) {
+            return item.text
+          })
+        }
+      })
+    },
     textUpdated(text: string) {
       this.title = text
     },
     getDetail() {
-      api.lightpage.getLightPageDetail({key:this.pageKey}).then((res)=>{
-        if(res.success){
+      api.lightpage.getLightPageDetail({ key: this.pageKey }).then((res) => {
+        if (res.success) {
           this.webpage = res.data
+          this.tags = (this.webpage?.plainData?.categories || this.webpage.tags).map(function(item) {
+            return {
+              text: item
+            }
+          })
         }
       })
     },
 
-    onEditLight(step:Step,index:number){
-      console.log(step,index,'修改')
-      const page = this.webpage;
-      if(!page){
+    onEditLight(step: Step, index: number) {
+      console.log(step, index, '修改')
+      const page = this.webpage
+      if (!page) {
         return
       }
-      if(step){
-        page.plainData.steps[index] = step;
-      }else{
-        page.plainData.steps.splice(index,1)
+      if (step) {
+        page.plainData.steps[index] = step
+      } else {
+        page.plainData.steps.splice(index, 1)
       }
-      this.save(page,false)
+      this.save(page, false)
     },
 
-    save(data:Partial<WebPage>,refresh=true){
+    save(data: Partial<WebPage>, refresh = true) {
       api.lightpage.saveLightPage({
         ...this.webpage,
         ...data,
         key: this.pageKey,
-        updateAt: Date.now(),
-      }).then(()=> {
-        if(refresh){
+        updateAt: Date.now()
+      }).then(() => {
+        if (refresh) {
           this.getDetail()
-          this.$emit('changed',this.pageKey)
+          this.$emit('changed', this.pageKey)
         }
       })
     },
 
-    deletePage(){
+    deletePage() {
       this.save({
         deleted: true,
         expiredAt: Date.now() + ONE_MONTH_MILLION
       })
     },
-    revert(){
+    revert() {
       this.save({
         deleted: false,
-        expiredAt: 0,
+        expiredAt: 0
       })
     }
   }
@@ -194,7 +225,8 @@ export default Vue.extend({
   box-shadow: 0 0 3px 1px #eae6e6;
   line-height: 1.4em;
   vertical-align: top;
-  .icon{
+
+  .icon {
     width: 24px;
     height: 24px;
     border-raduis: 10px;
@@ -202,18 +234,20 @@ export default Vue.extend({
     border-radius: 50%;
     background-color: #fff;
   }
-  .title{
+
+  .title {
     top: 0;
     padding: 8px;
     z-index: 1;
     position: sticky;
-    background: rgba(255,255,255,0.95);
+    background: rgba(255, 255, 255, 0.95);
     white-space: nowrap;
     border-bottom: 1px dotted #dddddd;
     border-radius: 8px 8px 0 0;
     overflow: hidden;
     text-overflow: ellipsis;
-    a{
+
+    a {
       font-size: 14px;
       display: inline-block;
       overflow: hidden;
@@ -223,7 +257,8 @@ export default Vue.extend({
       vertical-align: sub;
     }
   }
-  .actions{
+
+  .actions {
     width: 148px;
     position: absolute;
     display: flex;
@@ -232,17 +267,31 @@ export default Vue.extend({
     top: 12px;
     justify-content: space-between;
   }
-  .abstract{
-    padding:12px;
+
+  .abstract {
+    padding: 12px;
   }
 }
 
-aside.footer{
+aside.footer {
+  width: 100%;
   position: absolute;
   bottom: 0;
-  right: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding: 12px;
   font-size: 12px;
   color: #999;
+  background: #fff;
+}
+</style>
+
+<style>
+.ti-input {
+  border: none !important;
+  border-bottom: 1px solid #a4b1b6 !important;
+  padding: 4px 12px;
 }
 </style>
--- plugins/components.js
+++ plugins/components.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueSearchPanel from 'vue-search-panel'
 import VModal from 'vue-js-modal/dist/ssr.nocss'
 import KTabs from '@kevindesousa/vue-k-tabs'
+import VueTagsInput from '@johmun/vue-tags-input'
 
 import 'vue-js-modal/dist/styles.css'
 import VueCalendarHeatmap from '../components/heatmap'
@@ -10,3 +11,4 @@ Vue.use(VueCalendarHeatmap)
 Vue.use(VModal, {})
 Vue.use(KTabs)
 Vue.use(VueSearchPanel)
+Vue.use(VueTagsInput)
